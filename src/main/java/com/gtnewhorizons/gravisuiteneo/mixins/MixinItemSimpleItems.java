8fe248c2cc77182abce0e98d7243d2142729c4fa
glowredman <35727266+glowredman@users.noreply.github.com>
2023-01-25 22:20:12 +0100

    Fix item names

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemSimpleItems.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemSimpleItems.java
@@ -1,5 +1,6 @@
 package com.gtnewhorizons.gravisuiteneo.mixins;
 
+import com.gtnewhorizon.mixinextras.injector.ModifyReturnValue;
 import com.gtnewhorizons.gravisuiteneo.GraviSuiteNeo;
 import gravisuite.ItemSimpleItems;
 import net.minecraft.client.renderer.texture.IIconRegister;
@@ -17,8 +18,13 @@ public class MixinItemSimpleItems {
     private IIcon[] iconsList;
 
     @Inject(at = @At("HEAD"), method = "registerIcons")
-    private void registerAntidoteIcon(IIconRegister iconRegister, CallbackInfo ci) {
+    private void gravisuiteneo$registerAntidoteIcon(IIconRegister iconRegister, CallbackInfo ci) {
         this.iconsList = new IIcon[8];
         this.iconsList[7] = iconRegister.registerIcon(GraviSuiteNeo.MODID + ":itemAntidote");
+    }
+
+    @ModifyReturnValue(at = @At("RETURN"), method = "getUnlocalizedName")
+    private String gravisuiteneo$fixUnlocalizedName(String original) {
+        return "item." + original;
     }
 }
