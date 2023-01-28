959f82df5fa0d2cb2a845d88a8053479ec7d6a27
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-28 17:34:53 +0100

    inject inside the new Keybinding creation to avoid "dead keybind" registration to the keybind map

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinKeyboardClient.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinKeyboardClient.java
@@ -1,16 +1,18 @@
 package com.gtnewhorizons.gravisuiteneo.mixins;
 
 import gravisuite.keyboard.KeyboardClient;
-import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(KeyboardClient.class)
 public class MixinKeyboardClient {
-    @Shadow(remap = false)
-    public static KeyBinding flyKey = new KeyBinding("Gravi Fly Key", Keyboard.KEY_NONE, "GraviSuite");
 
-    @Shadow(remap = false)
-    public static KeyBinding displayHUDKey = new KeyBinding("Gravi Display Hud", Keyboard.KEY_NONE, "GraviSuite");
+    @ModifyConstant(
+            method = "<clinit>",
+            constant = {@Constant(intValue = 33), @Constant(intValue = 35)})
+    private static int gravineo$modifykeycode(int original) {
+        return Keyboard.KEY_NONE;
+    }
 }
