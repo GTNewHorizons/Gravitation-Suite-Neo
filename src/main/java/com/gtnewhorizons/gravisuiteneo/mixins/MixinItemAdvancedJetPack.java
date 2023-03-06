4238f375346324010833314ef4d690a02fc278db
glowredman <35727266+glowredman@users.noreply.github.com>
2023-03-06 17:15:50 +0100

    Remove hovermode falling

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemAdvancedJetPack.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemAdvancedJetPack.java
@@ -2,12 +2,19 @@ package com.gtnewhorizons.gravisuiteneo.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import com.gtnewhorizons.gravisuiteneo.common.Properties;
 import gravisuite.ItemAdvancedJetPack;
 
 @Mixin(ItemAdvancedJetPack.class)
 public class MixinItemAdvancedJetPack {
+
+    @ModifyConstant(constant = @Constant(doubleValue = 0.03), method = "<init>", remap = false)
+    private double gravisuiteneo$getHoverModeFallSpeed(double original) {
+        return Properties.AdvTweaks.getHoverModeFallSpeed();
+    }
 
     /**
      * @author Namikon, glowredman
