06bc800f7d6368effea125d69b4dc84cbfc33dd9
Tianyou Mei <meitianyou94@gmail.com>
2023-05-26 04:10:12 +0800

    changes the order of calls when mining in accurate/silk touch mode

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemVajra.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemVajra.java
@@ -2,17 +2,16 @@ package com.gtnewhorizons.gravisuiteneo.mixins;
 
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.gtnewhorizons.gravisuiteneo.common.Properties;
@@ -52,4 +51,35 @@ public class MixinItemVajra {
             @SuppressWarnings("rawtypes") List tooltip, boolean advancedTooltips, CallbackInfo ci) {
         tooltip.add(EnumChatFormatting.AQUA + StatCollector.translateToLocal("message.vajra.clickRightForSilk"));
     }
+
+    // the bottom redirect handles everything, so there is no need for these calls to do anything.
+    @Redirect(
+            method = "onItemUse",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/Block;onBlockHarvested(Lnet/minecraft/world/World;IIIILnet/minecraft/entity/player/EntityPlayer;)V",
+                    ordinal = 1))
+    private void onBlockHarvestToNoOp(Block block, World world, int x, int y, int z, int meta, EntityPlayer player) {
+        return;
+    }
+
+    @Redirect(
+            method = "onItemUse",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockToAir(III)Z", ordinal = 1))
+    private boolean setBlockToAirToNoOp(World world, int x, int y, int z) {
+        return true;
+    }
+
+    // harvestBlock would be called first by the vajra, so we redirect to the proper chain of calls
+    @Redirect(
+            method = "onItemUse",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/Block;harvestBlock(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/EntityPlayer;IIII)V"))
+    private void fixCallOrder(Block block, World world, EntityPlayer player, int x, int y, int z, int meta) {
+        block.onBlockHarvested(world, x, y, z, meta, player);
+        world.setBlockToAir(x, y, z);
+        block.harvestBlock(world, player, x, y, z, meta);
+    }
+
 }
