cf7341abee1180120e473bbe60136a755b83092a
TechnicianLP <9272536+TechnicianLP@users.noreply.github.com>
2023-07-03 02:21:12 +0200

    Fix EntityPlayer leak

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinKeyboard.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinKeyboard.java
@@ -1,10 +1,14 @@
 package com.gtnewhorizons.gravisuiteneo.mixins;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,6 +20,32 @@ import gravisuite.keyboard.Keyboard;
 
 @Mixin(Keyboard.class)
 public class MixinKeyboard {
+
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> boostKeyState;
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> jumpKeyState;
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> sneakKeyState;
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> forwardKeyState;
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> altKeyState;
+    @Shadow(remap = false)
+    private static Map<EntityPlayer, Boolean> modeKeyState;
+
+    /**
+     * Replace HashMap with WeakHashMap to prevent leaking EntityPlayer (and contained World) Objects.
+     */
+    @Inject(method = "<clinit>", at = @At("TAIL"), remap = false)
+    private static void gravisuiteneo$clinit(CallbackInfo ci) {
+        boostKeyState = new WeakHashMap<>();
+        jumpKeyState = new WeakHashMap<>();
+        sneakKeyState = new WeakHashMap<>();
+        forwardKeyState = new WeakHashMap<>();
+        altKeyState = new WeakHashMap<>();
+        modeKeyState = new WeakHashMap<>();
+    }
 
     @Inject(
             at = @At(
