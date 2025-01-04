3ff05d8b4869538a9a0dc4166d8b039b2b4b1689
Jakub <53441451+kuba6000@users.noreply.github.com>
2025-01-04 01:25:54 +0100

    Fix falling with enabled hover mode and holding both jump and sneak key (#24)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemAdvancedJetPack.java
+++ src/main/java/com/gtnewhorizons/gravisuiteneo/mixins/MixinItemAdvancedJetPack.java
@@ -2,10 +2,13 @@ package com.gtnewhorizons.gravisuiteneo.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import com.gtnewhorizons.gravisuiteneo.common.Properties;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 
 import gravisuite.ItemAdvancedJetPack;
 
@@ -33,5 +36,14 @@ public class MixinItemAdvancedJetPack {
     @Overwrite(remap = false)
     private double getBaseAbsorptionRatio() {
         return 1.0;
+    }
+
+    @ModifyExpressionValue(
+            method = "useJetpack",
+            at = @At(value = "CONSTANT", args = "doubleValue=-0.2"),
+            remap = false)
+    private static double gravisuiteneo$fixFallingWhenHoldingJumpAndSneak(double original,
+            @Local(name = "maxHoverY") double maxHoverY) {
+        return maxHoverY + original;
     }
 }
